5b7cae7bedddfb71d1519f4c5e95a8778bd05ca4
João Jamba <jambapedrojoao@gmail.com>
2023-08-27 23:51:44 +0100

    fetch data

--- tours/src/App.js
+++ tours/src/App.js
@@ -1,6 +1,50 @@
+import React, { useEffect, useState } from "react";
+import Loading from "./Loading";
+import Tours  from "./Tours"
 
+const url = 'https://course-api.com/react-tours-project'
 
 function App() {
+  const [leoading, setLoading ] = useState(true)
+  const [tours, setTours] = useState([])
+
+  const removeTour = (id) => {
+    const newTours = tours.filter((tour) => tour.id !== id)
+    setTours(newTours)
+  }
+
+  const fetchTours = async () => {
+    setLoading(true)
+
+    try{
+      const response = await fetch(url)
+      const data = await response.json()
+      console.log(data)
+      setLoading(false)
+      setTours(data)
+    }catch(error){
+      setLoading(false)
+      console.log(error)
+    }
+  }
+
+
+
+  useEffect(() => {
+    fetchTours()
+  },[])
+
+
+  if(leoading){
+    return(
+      <main>
+        <Loading/>
+      </main>
+    )
+  }
+
+  
+
   return (
     <div className="App">
       Fala dev
