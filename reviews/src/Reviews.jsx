510461e83da94ab8c52f359f0f97aec4037a7646
João Jamba <jambapedrojoao@gmail.com>
2023-09-11 01:21:30 +0100

    styled reviws

--- reviews/src/Reviews.jsx
+++ reviews/src/Reviews.jsx
@@ -45,7 +45,7 @@ const Reviews = () => {
         <article className="review">
         <div className="img-container">
             <img src={image} alt={name} className="person-img"/>
-            <span className=""quote-icon>
+            <span className="quote-icon">
                 <FaQuoteRight/>
             </span>
         </div>
