5ec6dec1cede2b446b60348fff32f229900e8abe
João Jamba <jambapedrojoao@gmail.com>
2023-12-01 01:45:32 +0100

    feat: styling menu with css

---
+++ menu/src/data.js
@@ -0,0 +1,76 @@
+const menu = [
+    {
+        id:1,
+        title: 'buttermilk pancakes',
+        category: 'breakfast',
+        price: 15.99,
+        img: 'https://images.unsplash.com/photo-1493770348161-369560ae357d?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        desc: 'Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos.'
+    },
+    {
+        id:2,
+        title: 'diner double',
+        category: 'lunch',
+        price: 12.99,
+        img: 'https://images.unsplash.com/photo-1538220856186-0be0e085984d?q=80&w=1526&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        desc: 'Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos.'
+    },
+    {
+        id:3,
+        title: 'godzilla milkshake',
+        category: 'shakes',
+        price: 6.99,
+        img: 'https://images.unsplash.com/photo-1513442542250-854d436a73f2?q=80&w=647&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        desc: 'Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos.'
+    },
+    {
+        id:4,
+        title: 'country delight',
+        category: 'breakfast',
+        price: 25.99,
+        img: 'https://images.unsplash.com/photo-1479803244419-b24dfe9cbafa?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        desc: 'Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos.'
+    },
+    {
+        id:5,
+        title: 'egg attack',
+        category: 'lunch',
+        price: 23.99,
+        img: 'https://images.unsplash.com/photo-1493770348161-369560ae357d?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        desc: 'Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos.'
+    },
+    {
+        id:6,
+        title: 'oreo dream',
+        category: 'shakes',
+        price: 18.99,
+        img: 'https://images.unsplash.com/photo-1464219222984-216ebffaaf85?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        desc: 'Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos.'
+    },
+    {
+        id:7,
+        title: 'bacon overflow',
+        category: 'breakfast',
+        price: 7.99,
+        img: 'https://images.unsplash.com/photo-1582169296194-e4d644c48063?q=80&w=700&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        desc: 'Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos.'
+    },
+    {
+        id:8,
+        title: 'american classic',
+        category: 'breakfast',
+        price: 13.99,
+        img: 'https://plus.unsplash.com/premium_photo-1661755506463-625fea429dc8?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        desc: 'Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos.'
+    },
+    {
+        id:9,
+        title: 'quarantine buddy',
+        category: 'shakes',
+        price: 16.99,
+        img: 'https://images.unsplash.com/photo-1599789197237-5866b38a5774?q=80&w=799&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        desc: 'Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos.'
+    }
+]
+
+export default menu;
